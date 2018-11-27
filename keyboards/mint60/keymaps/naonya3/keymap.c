2003dd3fac97c2c9a8a848ed465774a9446a8d2a
naonya3 <naoto.horiguchi@gmail.com>
2018-11-27 12:32:56 +0900

    Training mode

--- keyboards/mint60/keymaps/naonya3/keymap.c
+++ keyboards/mint60/keymaps/naonya3/keymap.c
@@ -38,9 +38,9 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT( \
-    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,    KC_BSPC,  \
+    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,    XXXXXXX,  \
     KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,  \
-    TG_LANG,     KC_A,    KC_S,    KC_D,    KC_F,    KC_G,      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,        KC_ENT,   \
+    TG_LANG,     KC_A,    KC_S,    KC_D,    KC_F,    KC_G,      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,        XXXXXXX,   \
     KC_LSFT,       KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,      KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,   KC_UP,  MO(1),   \
     KC_ESC,    KC_LCTL,    KC_LALT,    KC_LGUI,    KC_SPC,        KC_BSPC,   SFT_T(KC_ENT),  KC_LGUI,               KC_LEFT,KC_DOWN,KC_RGHT \
   ),
@@ -103,7 +103,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       if (record->event.pressed) {
         toggle_lang();
       }
-      break;
+      return false;
   }
   return true;
 }
@@ -138,3 +138,4 @@ void dance_lang (qk_tap_dance_state_t *state, void *user_data) {
 qk_tap_dance_action_t tap_dance_actions[] = {
   [TD_LANG] = ACTION_TAP_DANCE_FN(dance_lang)
 };
+
